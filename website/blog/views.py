aef86711aeb104200f6252654afadeea13aa5155
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2018-04-08 21:38:49 +0200

    commit last improvements
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, reverse
+from django.views import generic, View
 from django.utils import timezone
+from django.http import Http404, HttpResponseRedirect
 
 from .models import Post, Comment
 
 class IndexView(generic.ListView):
     template_name = 'blog/index.html'
     context_object_name = 'posts'
-
-    def get_queryset(self):
-        return Post.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
-
+    paginate_by = 5
+    queryset = Post.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
 
 class DetailView(generic.DetailView):
     model = Post
@@ -21,6 +20,7 @@ class DetailView(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['comments'] = Comment.objects.all()
+        post = context['post']
+        context['comments'] = Comment.objects.filter(post_id=post.id).order_by('-pub_date')
         return context
 
