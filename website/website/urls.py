38ef9d9986fa5e756fa75e7b5b2be7b07c774ee9
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2018-04-09 02:03:03 +0200

    add root redirect
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

--- website/website/urls.py
+++ website/website/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
+    path('', lambda _: HttpResponseRedirect('/blog')),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
